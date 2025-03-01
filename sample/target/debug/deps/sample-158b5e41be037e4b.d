c4b1401dd4b8d5a7d6bca7681f0961546ebcf46c
Ged <120242971+GedK0tlet@users.noreply.github.com>
2025-03-01 10:13:50 +0300

    init

---
+++ sample/target/debug/deps/sample-158b5e41be037e4b.d
@@ -0,0 +1,5 @@
+/Users/evgenii/Desktop/Develop/Rust/LerRust/rust_proj/sample/target/debug/deps/libsample-158b5e41be037e4b.rmeta: src/main.rs
+
+/Users/evgenii/Desktop/Develop/Rust/LerRust/rust_proj/sample/target/debug/deps/sample-158b5e41be037e4b.d: src/main.rs
+
+src/main.rs:
---
+++ sample/target/debug/deps/sample-9b5b15891eb40106.d
@@ -0,0 +1,5 @@
+/Users/evgenii/Desktop/Develop/Rust/LerRust/rust_proj/sample/target/debug/deps/libsample-9b5b15891eb40106.rmeta: src/main.rs
+
+/Users/evgenii/Desktop/Develop/Rust/LerRust/rust_proj/sample/target/debug/deps/sample-9b5b15891eb40106.d: src/main.rs
+
+src/main.rs:
